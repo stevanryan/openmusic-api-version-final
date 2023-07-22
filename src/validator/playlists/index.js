60507c5609fbfe3ab4dca901159636b602960a7c
stevanryan <stevanryan85@gmail.com>
2023-07-22 21:30:23 +0700

    adding new validator

---
+++ src/validator/playlists/index.js
@@ -0,0 +1,23 @@
+const { PlaylistPayloadSchema, PlaylistSongsPayloadSchema } = require('./schema');
+// Client Error exceptions.
+const InvariantError = require('../../exceptions/InvariantError');
+
+const PlaylistsValidator = {
+  validatePlaylistPayload: (payload) => {
+    const validationResult = PlaylistPayloadSchema.validate(payload);
+
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+
+  validatePlaylistSongsPayload: (payload) => {
+    const validationResult = PlaylistSongsPayloadSchema.validate(payload);
+
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = PlaylistsValidator;
---
+++ src/validator/users/index.js
@@ -0,0 +1,15 @@
+const { UserPayloadSchema } = require('./schema');
+// Client Error exceptions.
+const InvariantError = require('../../exceptions/InvariantError');
+
+const UsersValidator = {
+  validateUserPayload: (payload) => {
+    const validationResult = UserPayloadSchema.validate(payload);
+
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = UsersValidator;
