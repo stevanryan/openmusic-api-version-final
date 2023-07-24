b84b94c2691ec7e774f436180fb2ef5d510e8360
stevanryan <stevanryan85@gmail.com>
2023-07-24 16:19:54 +0700

    removing some comments

--- src/tokenize/TokenManager.js
+++ src/tokenize/TokenManager.js
@@ -3,9 +3,7 @@ const InvariantError = require('../exceptions/InvariantError');
 
 const TokenManager = {
   // Parameter payload merupakan objek yang disimpan ke dalam salah satu artifacts JWT.
-  // Biasanya berisi properti yang mengindikasikan identitas pengguna, seperti user id.
   generateAccessToken(payload) {
-    // generate menerima dua parameter payload dan secretKey.
     return Jwt.token.generate(payload, process.env.ACCESS_TOKEN_KEY);
   },
   generateRefreshToken(payload) {
